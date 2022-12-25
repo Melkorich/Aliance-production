1d2dcaaf16f72039e5a73eb3b43cefc7c473c725
kirill lavrov <kirill.lavrov92@gmail.com>
2022-12-25 20:21:58 +0300

    added: icons svg sprites

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const navbar = document.querySelector('.navbar');
+const logo = document.querySelector('.logo-svg use');
 
 window.addEventListener('scroll', () => {
     if (this.scrollY > 1) {
-        navbar.classList.add('navbar--light')
+        navbar.classList.add('navbar--light');
+        logo.href.baseVal = "images/sprite.svg#logo-dark";
     } else {
         navbar.classList.remove('navbar--light')
+        logo.href.baseVal = "images/sprite.svg#logo-light";
     }
 })
