f9170bd7c807eadbd237b1f67c916cb4efeb01ba
kirill lavrov <kirill.lavrov92@gmail.com>
2022-12-28 18:25:58 +0300

    add: menu btn js + transition

--- js/main.js
+++ js/main.js
@@ -1,22 +1,42 @@
 const navbar = document.querySelector('.navbar');
 const logo = document.querySelector('.logo-svg use');
 
+
+const lightModeOn = () => {
+    navbar.classList.add('navbar--light');
+    logo.href.baseVal = "images/sprite.svg#logo-dark";
+}
+
+const lightModeOff = () => {
+    navbar.classList.remove('navbar--light')
+    logo.href.baseVal = "images/sprite.svg#logo-light";
+}
+
 window.addEventListener('scroll', () => {
-    if (this.scrollY > 1) {
-        navbar.classList.add('navbar--light');
-        logo.href.baseVal = "images/sprite.svg#logo-dark";
-    } else {
-        navbar.classList.remove('navbar--light')
-        logo.href.baseVal = "images/sprite.svg#logo-light";
-    }
+    this.scrollY > 1 ? lightModeOn() : lightModeOff();
 })
+
 
 const mobileMenuBtn = document.querySelector('.mobile-nav');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const openMenu = () => {
+    mobileMenu.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
+    mobileMenuBtn.classList.add('mobile-nav--active');
+
+    lightModeOn();
+}
+
+const closeMenu = () => {
+    mobileMenu.classList.remove('is-open');
+    document.body.style.overflow = '';
+    mobileMenuBtn.classList.remove('mobile-nav--active');
+
+    lightModeOff();
+}
 
 mobileMenuBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    mobileMenu.classList.toggle('is-open');
-    mobileMenuBtn.classList.toggle('mobile-nav--active');
+    mobileMenu.classList.contains('is-open') ? closeMenu() : openMenu();
 })
